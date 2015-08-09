b080ee90df4e355385fa78fadf029c80b9861e0a
Dylan Sanders <sandersd27@gmail.com>
2015-08-09 01:47:39 -0700

    tap

--- pebbleCode.js
+++ pebbleCode.js
@@ -1,2 +1,19 @@
 simply.title('Hello World!');
 simply.vibe('short');
+
+var count = parseInt(localStorage.getItem('count')) || 0;
+
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    simply.subtitle(++count);
+  } else if (e.button === 'down') {
+    simply.subtitle(--count);
+  }
+  localStorage.setItem('count', count);
+});
+
+simply.text({ title: 'Counter', subtitle: count });
+
+simply.on('accelTap', function(e) {
+  simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+});
